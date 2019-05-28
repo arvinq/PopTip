44a4d4f749bcfe3e18028640b8a49a4bee1f3e9b
arvnq <arvin.quiliza@gmail.com>
2019-05-28 10:58:06 +1000

    ToolTipControl before being a framework

---
+++ PopTip/App/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  PopTip
+//
+//  Created by Arvin Quiliza on 5/25/19.
+//  Copyright © 2019 arvinq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        showToolTip()
+    }
+
+    @IBAction func showPopup(_ sender: UIView) {
+        
+//        let controller = PopTip()
+//        controller.showToolTip(onItem: sender)
+//        self.present(controller, animated: true)
+        
+    }
+    
+    func showToolTip() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) { [weak self] in
+            guard let strongSelf = self else { return }
+            // do PopTip here
+            let controller = PopTip()
+            controller.showToolTip(onItem: strongSelf.button)
+            strongSelf.present(controller, animated: true)
+        }
+    }
+    
+    
+}
